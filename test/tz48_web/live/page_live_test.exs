2b6fcc0892a3da1bf9b0487ce15818c0330c1865
Shankar Dhanasekaran <shankardevy@gmail.com>
2020-11-07 17:48:29 +0530

    Remove default phoenix liveview code

--- test/tz48_web/live/page_live_test.exs
+++ test/tz48_web/live/page_live_test.exs
@@ -3,9 +3,5 @@ defmodule TZ48Web.PageLiveTest do
 
   import Phoenix.LiveViewTest
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
-  end
+
 end
